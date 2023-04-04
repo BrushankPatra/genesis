f21b53de5ce27ab8056f9d18f717b28f3528503c
BrushankPatra <72485994+BrushankPatra@users.noreply.github.com>
2023-04-04 13:37:44 +0530

    ListView() used for transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,50 +11,53 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((txn) {
-        return Card(
-          elevation: 5,
-          child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                child: Text(
-                  "₹ ${txn.amount}",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 16,
+    return Container(
+      height: 600,
+      child: ListView(
+        children: transactions.map((txn) {
+          return Card(
+            elevation: 5,
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
                   ),
-                ),
-                decoration: BoxDecoration(color: Colors.indigo),
-                padding: EdgeInsets.all(10),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(txn.title,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.indigo,
-                        fontWeight: FontWeight.bold,
-                      )),
-                  Text(
-                    DateFormat("yMMMMEEEEd").format(txn.date),
+                  child: Text(
+                    "₹ ${txn.amount}",
                     style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey[800],
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 16,
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                  decoration: BoxDecoration(color: Colors.indigo),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(txn.title,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.indigo,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Text(
+                      DateFormat("yMMMMEEEEd").format(txn.date),
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey[800],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
