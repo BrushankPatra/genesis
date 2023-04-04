c0eb6002843987912ccda310717f7d4d9406b72d
BrushankPatra <72485994+BrushankPatra@users.noreply.github.com>
2023-04-04 15:02:38 +0530

    add buttons placed on appBar and body

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -37,6 +43,11 @@ class MyHomePage extends StatelessWidget {
             UserTransaction(),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
