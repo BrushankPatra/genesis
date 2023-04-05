936c9b4f9d8f28d7a29cf4dcf5b3cd8b8d799de8
BrushankPatra <72485994+BrushankPatra@users.noreply.github.com>
2023-04-05 21:59:25 +0530

    chart_bar coded and stylised

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,6 +9,37 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        Text("₹$spendingAmount"),
+        SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPrcntOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(label),
+      ],
+    );
   }
 }
