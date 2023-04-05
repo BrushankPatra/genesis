552afd3ea1767102c4c090db661c147c7b1bb546
BrushankPatra <72485994+BrushankPatra@users.noreply.github.com>
2023-04-05 21:32:56 +0530

    chart_bar.dart file created

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final int spendingAmount;
+  final double spendingPrcntOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPrcntOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
